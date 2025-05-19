52c0e5b9115b2137155af8d5f46113cfc8e3e633
dixitmalviya10 <dixitmalviya10@gmail.com>
2025-05-19 23:17:59 +0530

    feat: converted into a PWA app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from './manifest';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA(manifestForPlugIn)
   ],
 })
